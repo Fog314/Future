710cde2a579aeef773faa3e650948d4593b665ec
Fog314 <navimed7@gmail.com>
2020-03-04 22:28:53 +0300

    Rendering table

---
+++ src/Test.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+let newTableObject = document.querySelector('.sortable');
+console.log(newTableObject);
+
+class Test extends React.Component{
+    render(){
+        return(
+            <div>Hello</div>
+        )
+    }
+}
+
+
+export default Test
